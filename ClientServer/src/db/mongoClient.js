cce96076f665ade2bb8b7367a660b3b5f4d4b1c4
mattmulli <matthewunderscoremullin@gmail.com>
2023-09-25 18:58:29 -0400

    mongo client correction 3

--- ClientServer/src/db/mongoClient.js
+++ ClientServer/src/db/mongoClient.js
@@ -8,11 +8,17 @@ export function mongoClient(DB_URI) {
 export const connect = (mongoClient) => new Promise((resolve,reject) => {
     mongoClient.connect()
     .catch((err) => reject(err))
-    .then(() => resolve("connected"));
+    .then(() => {
+        console.log('db connected');
+        resolve("connected")
+    });
 });
 
 export const disconnect = (mongoClient) => new Promise((resolve,reject) => {
     mongoClient.close()
     .catch((err) => reject(err))
-    .then(() => resolve("disconnected"));
+    .then(() => {
+        console.log('db disconnected');
+        resolve("disconnected")
+    });
 });
