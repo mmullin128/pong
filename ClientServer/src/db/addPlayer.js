cbc1e44a6f537625768a04da49b2856eb4d897db
mattmulli <matthewunderscoremullin@gmail.com>
2023-09-26 23:10:30 -0400

    correction 1

--- ClientServer/src/db/addPlayer.js
+++ ClientServer/src/db/addPlayer.js
@@ -1,11 +1,7 @@
 
 export async function addPlayer(mongoClient,collectionNum,playerData,playersCollections) {
     //adds player to appropriate collection recursively
-    if (!playersCollections) {
-        const metaCollection = mongoClient.db("DB1").collection("Meta");
-        const meta = await metaCollection.findOne({});
-        playersCollections = meta["playersCollections"];
-    }
-    //
+    //add player to collection and increment meta
+    
 
 }
