767acd45fdf2dbe2db0943089425f6cb254f96b7
mattmulli <matthewunderscoremullin@gmail.com>
2023-09-27 20:47:27 -0400

    correction 5

--- ClientServer/src/db/updatePlayer.js
+++ ClientServer/src/db/updatePlayer.js
@@ -1,7 +1,7 @@
 import { getCollectionMeta } from "./getCollectionMeta.js";
 export async function updatePlayer(mongoClient,collectionCode,id,playerData) {
     const collectionData = await getCollectionMeta(mongoClient,'Players',0,collectionCode);
-    const playersCollection = mongoClient.db("DB1").collection(collectionData.collectionName);
+    const playersCollection = mongoClient.db("DB1").collection(collectionData.name);
     await playersCollection.updateOne(
         {
             "id": id
